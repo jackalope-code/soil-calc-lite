1b70121968a22af93f85634b549e4affab54cbbd
Sean Phelan <seanphelan97@gmail.com>
2021-03-14 20:12:51 +0100

    fix event logic

--- main.js
+++ main.js
@@ -5,35 +5,59 @@ let bagSize = 0;
 const volumeDisplay = document.getElementById('calculated-volume');
 const numBagsDisplay = document.getElementById('calculated-bags');
 
-const onTextChange = (event) => {
-  const element = event.target;
+const fieldMap = new Map();
+const fields = [
+  'fill-length-feet', 'fill-length-inches',
+  'fill-width-feet', 'fill-width-inches',
+  'fill-height-feet', 'fill-height-inches',
+  'bag-size'
+];
 
-  switch(element.id) {
-    case 'fill-length':
-      length = element.value || 0;
-      break;
-    case 'fill-width':
-      width = element.value || 0;
-      break;
-    case 'fill-height':
-      height = element.value || 0;
-      break;
-    case 'bag-size':
-      bagSize = element.value || 0;
-      break;
+fields.map((fieldName) => {
+  // TODO: this is just kind of a hack for an array of default values
+  let defaultValue = undefined;
+  if(fieldName.includes('inches')) {
+    defaultValue = 0;  
   }
 
-  // Update calculated volume
-  let volume = length * width * height;
-  volumeDisplay.innerHTML = volume > 0 ? `${volume} ft<sup>3</sup>` : 'Not specified';
+  fieldMap.set(fieldName, defaultValue);
+  // TODO: This could go into a component.
+  document.getElementById(fieldName).value = defaultValue;
+});
+
+const handleInputChange = (event) => {
+  const element = event.target;
+
+  // Update field values for any accepted input id
+  if(fieldMap.has(element.id)) {   
+    fieldMap.set(element.id, Number(element.value))
+  }
+
+  let length = fieldMap.get('fill-length-feet') + fieldMap.get('fill-length-inches')/12;
+  let width = fieldMap.get('fill-width-feet') + fieldMap.get('fill-width-inches')/12;
+  let depth = fieldMap.get('fill-height-feet') + fieldMap.get('fill-height-inches')/12;
+
+  // Update with volume of a rectangle
+  calculateAndUpdateBagCount(length*width*depth)
+}
+
+// Update calculated volume and display
+function calculateAndUpdateBagCount(volume) {
+
+  let bagSize = fieldMap.get('bag-size');
+
+  // Show updated volume on the screen
+  let volumeDisplayText = volume.toFixed(2) > 0 ? `${volume.toFixed(2)} ft<sup>3</sup>` : 'Not specified';
+  volumeDisplay.innerHTML = volumeDisplayText;
 
   // Update the calculated value when any of the fields change
-  let numBags = volume/bagSize;
+  let numBags = volume / bagSize;
   let outputText;
-  if(bagSize === 0) {
-    outputText = "Please specify a bag volume."
+  if (bagSize === 0) {
+    outputText = "Please specify a bag volume.";
   } else {
     outputText = numBags || 'Unknown';
   }
-  numBagsDisplay.innerHTML = outputText;
+  // Show updated number of bags on the screen
+  numBagsDisplay.innerHTML = outputText.toFixed(2);
 }
